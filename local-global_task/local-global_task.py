ed9eed7e72947e1cb962e0d0be42c8037cbd9749
Erik Marsja <erik@marsja.se>
2016-07-02 19:34:30 +0200

    Removed some useless experiments and just leaving the ones that may work.

--- local-global_task/local-global_task.py
+++ local-global_task/local-global_task.py
@@ -33,19 +33,17 @@ the example above press 'q' (circle) .
 mental set.  This quasi-randomization is done for each participant in
  contrast to prerandomized as in the paper.
 """
-import os, re
-from collections import Counter
-from random import shuffle
-
 from psychopy import visual, event ,core,data,gui
+import os, re
 from fileHandling import *
+from collections import Counter
 
 class Window():
     
     def __init__(self, name = "Local-Global Task"):
         self.name = name
         
-    def create_window(self, color):
+    def createWindow(self, color):
         '''For creating the experiment window
         in preferred color
         self.color = color
@@ -57,17 +55,17 @@ class Window():
             blendMode=u'avg',winType=u'pyglet')     
         return self.win
         
-    def get_subj_information(self, expName = "SET EXPERIMENT NAME"):
+    def experimentInfo(self, expName = "SET EXPERIMENT NAME"):
         self.expName = expName   
-        self.subj_information = {'Subject Id':'', 'Age':'', 
-                'Experiment Version': 0.1, 'Sex': ['Male', 'Female', 'Other']}
-        self.subj_information[u'date'] = data.getDateStr(format="%Y-%m-%d_%H:%M")  
-        self.infoDlg = gui.DlgFromDict(dictionary=self.subj_information, 
+        self.expInfo = {'Subject Id':'', 'Age':'', 'Experiment Version': 0.1,
+                        'Sex': ['Male', 'Female', 'Other']}
+        self.expInfo[u'date'] = data.getDateStr(format="%Y-%m-%d_%H:%M")  
+        self.infoDlg = gui.DlgFromDict(dictionary=self.expInfo, 
                                        title=self.expName,
                                        fixed=['Experiment Version'])
-        self.subj_information[u'DataFile'] = u'Data' + os.path.sep + u'DATA_Local-Global_Task.csv'
+        self.expInfo[u'DataFile'] = u'Data' + os.path.sep + u'DATA_Local-Global_Task.csv'
         if self.infoDlg.OK:
-            return self.subj_information
+            return self.expInfo
         else: 
             return 'Cancelled'
             
@@ -76,7 +74,7 @@ class Task():
         self.xTrials = nExpTrials
         self.pTrials = nPracTrials
 
-    def randomize_targets(self,listostim, ntrials):
+    def randomizeTargets(self,targets, ntrials):
         '''
         Will randomize 50 % of shifting trials
         based on that each filename (of the images) starts with Black or Blue-.
@@ -84,112 +82,111 @@ class Task():
         listostim is a list of trials
         ntrials is an integer indicating number of trials
         '''
-        stimList = listostim
-        nEachstim = ntrials/len(stimList)
-        nEachtype = ntrials/2
+        nEachstim = ntrials/len(targets)
+        wantedNeachType = ntrials/2
         trialTypes = []
-        stims = []
-        countOfStim = dict((el,0) for el in stimList)
-        count = {'ns':0,'s':0}
-        returnList = []
+        targetTrials = []
+        countOfTargets = dict((el,0) for el in targets)
+        countOfTrialTypes = {'ns':0,'s':0}
+        targetsAndTrialTypes = []
+        
         for i in range(ntrials):
-            shuffle(stimList)
-            for idx in range(len(stimList)):
-                if not stims:
-                    countOfStim[stimList[idx]] +=1
-                    stims.append(stimList[idx])
-                    count['ns'] +=1
+            shuffle(targets)
+            for idx in range(len(targets)):
+                if not targetTrials:
+                    countOfTargets[targets[idx]] +=1
+                    targetTrials.append(targets[idx])
+                    countOfTrialTypes['ns'] +=1
                     trialTypes.append("No-Shifting")
-                elif stims:
-                    if count['s'] <= nEachtype:
-                        if countOfStim[stimList[idx]] <= nEachstim-1:
-                            if stimList[idx][:5] != stims[i-1][:5]:
-                                count['s'] +=1
-                                countOfStim[stimList[idx]] +=1
-                                stims.append(stimList[idx])
+                elif targetTrials:
+                    if countOfTrialTypes['s'] <= wantedNeachType:
+                        if countOfTargets[targets[idx]] <= nEachstim-1:
+                            if targets[idx][:5] != targetTrials[i-1][:5]:
+                                countOfTrialTypes['s'] +=1
+                                countOfTargets[targets[idx]] +=1
+                                targetTrials.append(targets[idx])
                                 trialTypes.append("Shifting")
             
                             else:
-                                count['ns'] +=1
-                                countOfStim[stimList[idx]] +=1
-                                stims.append(stimList[idx])
+                                countOfTrialTypes['ns'] +=1
+                                countOfTargets[targets[idx]] +=1
+                                targetTrials.append(targets[idx])
                                 trialTypes.append("No-Shifting")
-                    elif count['s'] > nEachtype:
-                        if countOfStim[stimList[idx]] <= nEachstim-1: 
-                            if stimList[idx][:5] == stims[i-1][:5]:
-                                count['ns'] +=1
-                                countOfStim[stimList[idx]] +=1
-                                stims.append(stimList[idx])
+                    elif countOfTrialTypes['s'] > wantedNeachType:
+                        if countOfTargets[targets[idx]] <= nEachstim-1: 
+                            if targets[idx][:5] == targetTrials[i-1][:5]:
+                                countOfTrialTypes['ns'] +=1
+                                countOfTargets[targets[idx]] +=1
+                                targetTrials.append(targets[idx])
                                 trialTypes.append("No-Shifting")
+                                
         #Frequency of the trialtypes
-        freq = Counter(trialTypes).values()
-        if freq[0] == freq[1] and sum(freq) == ntrials:
-            returnList.append(stims)
-            returnList.append(trialTypes)
-            return returnList
-        elif freq[0] != nEachtype and freq[1] !=nEachtype or sum(freq) !=ntrials:
-            return self.randomize_targets(stimList, ntrials)
+        nEachTrialType = Counter(trialTypes).values()
+        if nEachTrialType[0] == nEachTrialType[1] and sum(nEachTrialType) == ntrials:
+            targetsAndTrialTypes.append(targetTrials)
+            targetsAndTrialTypes.append(trialTypes)
+            return targetsAndTrialTypes
+        elif nEachTrialType[0] != wantedNeachType and nEachTrialType[1] !=wantedNeachType or sum(nEachTrialType) !=ntrials:
+            return self.randomizeTargets(targets, ntrials)
     
-    def create_trials(self, trialsAndTypes, expinfo, files, practice=False):
+    def createExperimentTrials(self, trialsAndTypes, expinfo, files, practice=False):
         '''
-        Creates the experimental trials
-        uses PsychoPy's trialhandler
+        Uses PsychoPy's trialhandler
         trialsAndTypes is a list containing 2 lists. First is a list of strings
         for the Targets. Second is a list of TrialTypes
         expinfo contains the info of the experiment to be saved,
         files is the object with the images
         '''
         trialTypes = trialsAndTypes[1]
-        self.exp = expinfo
-        trials = trialsAndTypes[0]
-        trialList = []
-        listOfResps = ['q','w','e', 'r']
+        trialTargets = trialsAndTypes[0]
+        experimentTrials = []
+        responseKeys = ['q','w','e', 'r']
         if practice:
                 expTask = "Practice"
         else:
                 expTask = "Experimental"
                 
-        for trial, target in enumerate(trials):#Here i could use enumerate(trial, trialSeq)
+        for trialNumber, target in enumerate(trialTargets):
             parts = re.findall(r"[^\W_']+", target)
             if parts[0] == "Blue":
                 taskType = 'Global'
                 if parts[1] == "Circle":
-                    self.correctResp = listOfResps[0]
+                    correctResponse = responseKeys[0]
                 elif parts[1] == "X":
-                    self.correctResp = listOfResps[1]
+                    correctResponse = responseKeys[1]
                 elif parts[1] == "Triangle":
-                    self.correctResp = listOfResps[2]
+                    correctResponse = responseKeys[2]
                 elif parts[1] == "Rect":
-                    self.correctResp = listOfResps[3]
+                    correctResponse = responseKeys[3]
             elif parts[0] == "Black":
                 taskType = 'Local'
                 if parts[2] == "Circle":
-                    self.correctResp = listOfResps[0]
+                    correctResponse = responseKeys[0]
                 elif parts[2] == "X":
-                    self.correctResp = listOfResps[1]
+                    correctResponse = responseKeys[1]
                 elif parts[2] == "Triangle":
-                    self.correctResp = listOfResps[2]
+                    correctResponse = responseKeys[2]
                 elif parts[2] == "Rect":
-                    self.correctResp = listOfResps[3]
+                    correctResponse = responseKeys[3]
 
-            trialList.append({'Cresp':self.correctResp, 
+            experimentTrials.append({'Cresp':correctResponse, 
                                   u'File':files[target][1],
                                    u'Stimulus':files[target][0], 
-                                   u'Trial':trial+1,
-                                   u'Sub_id':self.exp['Subject Id'],
-                                     u'Age':self.exp['Age'], 
-                                     u'Sex':self.exp['Sex'], 
+                                   u'Trial':trialNumber+1,
+                                   u'Sub_id':expinfo['Subject Id'],
+                                     u'Age':expinfo['Age'], 
+                                     u'Sex':expinfo['Sex'], 
                                         u'TaskType':taskType, 
                                         u'Task':expTask,
-                                        u'TrialType':trialTypes[trial],
-                                     u'Date':self.exp['date']})
-        trialHandler = data.TrialHandler(trialList,1, method="sequential")  
+                                        u'TrialType':trialTypes[trialNumber],
+                                     u'Date':expinfo['date']})
+        trialHandler = data.TrialHandler(experimentTrials,1, method="sequential")  
         trialHandler.data.addDataType('Response')
         trialHandler.data.addDataType('Accuracy')
         trialHandler.data.addDataType('RT')  
         return trialHandler
     
-    def run_trials(self, trialObj, frameR, win, datafile,load):
+    def runTrials(self, trialObj, frameR, win, datafile,load):
         frameR = frameR
         trialhandler = trialObj
         self.win = win
@@ -200,9 +197,9 @@ class Task():
             self.timer.reset()
             for frame in range(self.targetFrames):
                     visualTarget = trial['Stimulus']
-                    self.present_stimulus('image', visualTarget)
+                    self.presentStimulus('image', visualTarget)
 
-            self.present_stimulus('text', '+')
+            self.presentStimulus('text', '+')
             keys = event.waitKeys(keyList=['q','w','a', 's'])
             if keys:  
                 trial['RT'] = self.timer.getTime()
@@ -214,9 +211,9 @@ class Task():
                 trial['RT'] = self.timer.getTime()
                 load.writeCsv(fileName=datafile, thisTrial=trial)
             for frame in range(self.itiFrames):
-                self.present_stimulus('text', '+')
+                self.presentStimulus('text', '+')
 
-    def create_text_stimuli(self, win, text, pos, name, height, color):
+    def createTextStimulus(self, win, text, pos, name, height, color):
         '''Creates a text stimulus,
         self.text = text
         self.pos = pos
@@ -235,11 +232,11 @@ class Task():
             color=self.color, colorSpace=u'rgb') 
         return textStim           
     
-    def present_stimulus(self, stim, target):
+    def presentStimulus(self, stim, target):
         self.stim = stim
         self.target = target
         self.win.flip()  
-        textOnScreen = self.create_text_stimuli(self.win, text='',
+        textOnScreen = self.createTextStimulus(self.win, text='',
                                      pos=[0.0,0.0], name='Visual Target',
                                      height=0.07, color='Black')
         if self.stim == "text":
@@ -247,39 +244,41 @@ class Task():
             textOnScreen.draw()
         elif self.stim == "image":
             self.target.draw()
-    def instructions(self, win):
+    def waitForKyes(self, win):
         win.flip()
         event.waitKeys()
         event.clearEvents()
         core.wait(.2)
 
 def main():
-    winz = Window()
-    info = winz.get_subj_information(expName = "Local-Global Task")
+    experimentWindow = Window()
+    info = experimentWindow.experimentInfo(expName = "Local-Global Task")
     load = fileHandling()
     load.makeDir('Data')
     task = Task(96,40)
-    win = winz.create_window(color="white")
-    frameR = win.getActualFrameRate()
+    win = experimentWindow.createWindow(color="white")
+    frameRate = win.getActualFrameRate()
     files = load.loadFiles('stimuli', '.png', 'image', win)
-    keyList = [key for key in files]
+    visualTargets = [key for key in files]
     txtfile = load.loadFiles("instructions", "txt", "text", win)
     txtfile['instruk'].draw()
-    task.instructions(win)
+    task.waitForKyes(win)
     #Practice Trials
-    pracList = task.randomize_targets(keyList, 40)
-    pracHand = task.create_trials(pracList, info, files,  practice=True)
-    task.run_trials(pracHand, frameR, win, info['DataFile'], load)
+    practiceTargets = task.randomizeTargets(visualTargets, 40)
+    pracHand = task.createExperimentTrials(practiceTargets, info, 
+                                           files,  practice=True)
+    task.runTrials(pracHand, frameR, win, info['DataFile'], load)
     core.wait(.5)
     txtfile['exptask'].draw()
-    task.instructions(win)
-    #Experimental Trials
-    stimList = task.randomize_targets(keyList, 96)
-    trialhand =  task.create_trials(stimList, info, files)
-    task.run_trials(trialhand, frameR, win, info['DataFile'], load)
+    task.waitForKyes(win)
+    #Experimental (test) Trials
+    testTargets = task.randomizeTargets(visualTargets, 96)
+    trialhand =  task.createExperimentTrials(testTargets, info, files)
+    task.runTrials(trialhand, frameRate, win, info['DataFile'], load)
     core.wait(.5)
     txtfile['thanks'].draw()
-    task.instructions(win)
+    task.waitForKyes(win)
     core.quit()
+    
 if __name__ == "__main__":
     main()        
